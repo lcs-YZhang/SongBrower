c94971db4e8420cf1f6d4a0f27b84881e5b4970e
Steven Zhang <YZhang@lcs.on.ca>
2021-02-20 10:15:56 -0500

    We define a model for the part of Json file we care about retrieving

---
+++ SongBrower/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  SongBrower
+//
+//  Created by Steven Zhang on 2021-02-20.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    
+    let results: [Song]
+    
+}
---
+++ SongBrower/Model/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  SongBrower
+//
+//  Created by Steven Zhang on 2021-02-20.
+//
+
+import Foundation
+
+struct Song: Decodable {
+    
+    let trackId: Int
+    let artistName: String
+    let trackName: String
+    let previewUrl: String
+    let artworkUrl100: String
+    
+}
