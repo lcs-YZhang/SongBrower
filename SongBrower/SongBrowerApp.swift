965d65e6e02d5e1edc37f2f793862afdb60764a4
Steven Zhang <YZhang@lcs.on.ca>
2021-02-20 09:49:37 -0500

    Initial Commit

---
+++ SongBrower/SongBrowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBrowerApp.swift
+//  SongBrower
+//
+//  Created by Steven Zhang on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SongBrowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
