b56b372b67f0f46aa2b5e1a03e9217e04358cb69
Steven Zhang <YZhang@lcs.on.ca>
2021-02-22 11:37:30 -0500

    Add four view that we will used to composed together for the song detail view

---
+++ SongBrower/Views/RemoteDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RemoteDetailView.swift
+//  SongBrower
+//
+//  Created by Steven Zhang on 2021-02-22.
+//
+
+import SwiftUI
+
+struct RemoteDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RemoteDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RemoteDetailView()
+    }
+}
